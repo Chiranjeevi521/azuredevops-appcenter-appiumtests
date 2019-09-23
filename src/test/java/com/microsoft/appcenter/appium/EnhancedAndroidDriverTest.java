eb84d1bcf9db7dd87fbedc2a07a7ab07cd2b85ee
Chiranjeevi Dasatti <chiranjeevi.dasatti@planonsoftware.com>
2019-09-23 19:09:08 +0530

    Added Basic Hardware Tests

--- src/test/java/com/microsoft/appcenter/appium/EnhancedAndroidDriverTest.java
+++ src/test/java/com/microsoft/appcenter/appium/EnhancedAndroidDriverTest.java
@@ -3,6 +3,7 @@ package com.microsoft.appcenter.appium;
 import org.junit.*;
 
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -19,12 +20,14 @@ import io.appium.java_client.MobileElement;
 import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
 import org.junit.rules.TestWatcher;
 import org.junit.Rule;
+import java.time.Duration;
 
 public class EnhancedAndroidDriverTest {
 
     public static final byte[] PNG = OutputType.BYTES.convertFromBase64Png("amZsanNmbGtkc2pmbGtzZGpmbGtkc2ZqbGtkc2ZqZGxza2ZqZHNsa2Zq");
     private static AppiumMock appiumMock;
     private MemoryEventReporter reporter = new MemoryEventReporter();
+    Duration fiveSeconds = Duration.ofSeconds(5);
 
 //    @Rule
 //    public Watcher watcher = new Watcher(reporter);
@@ -56,13 +59,6 @@ public class EnhancedAndroidDriverTest {
         driver.quit();
     }
 
-    @Test
-    public void testMark() throws Exception {
-        driver.label("Lallerkok");
-        for (String s : reporter.getIds()) {
-            System.out.println(s);
-        }
-    }
 
     @Test
     public void testGetScreenshotAs() throws Exception {
@@ -70,35 +66,38 @@ public class EnhancedAndroidDriverTest {
 
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testEmptyLabelsNotAllowed() {
-    	System.out.println("Current context is : " + driver.getContext());
-        driver.label("");
-    }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testNullLabelsNotAllowed() {
-        driver.label(null);
+    @Test 
+    public void screenLockTest() throws InterruptedException {
+    	
+    	driver.lockDevice();
+    	System.out.println("Device Lock Status : " + driver.isDeviceLocked());
+    	driver.unlockDevice();
     }
+    
+    
+  @Test	
+  public void runAppBackground() throws InterruptedException {
+  	
+	  driver.runAppInBackground(fiveSeconds);
+	  driver.closeApp();
+	  driver.launchApp();
+  	}
+    
+    
+  @Test
+  public void resetApp() throws InterruptedException {
+  	
+	  driver.resetApp();
+  	}
+    
+    
+  @Test 	
+  public void orientationTest() throws InterruptedException {
+  	driver.rotate(ScreenOrientation.LANDSCAPE);
+  	Thread.sleep(5000);
+  	driver.rotate(ScreenOrientation.PORTRAIT);
+  	Thread.sleep(10000);
+  	}
 
-    @Test
-    public void test256CharLabelsAreAllowed() {
-        String longLabel = createLabelOfSize(256);
-        driver.label(longLabel);
-        //expected 128 to be allowed
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testVeryLongLabelsAreNotAllowed() {
-        String longLabel = createLabelOfSize(257);
-        driver.label(longLabel);
-    }
-
-    private String createLabelOfSize(int i) {
-        StringBuilder sb = new StringBuilder(i);
-        while (i-- > 0) {
-            sb.append("a");
-        }
-        return sb.toString();
-    }
 }
